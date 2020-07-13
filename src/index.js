cc5793fa1160cf4f6f4260f8d6a3c9ebe76a5657
David Colatti <davidcolatti@gmail.com>
2020-07-13 15:31:36 -0400

    feat: graphql server

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+// import { typeDefs, resolvers } from "./schema";
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "hello",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => {
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+});
